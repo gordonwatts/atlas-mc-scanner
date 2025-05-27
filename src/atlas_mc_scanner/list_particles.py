3abfdeeb136715985a4f8fa9de23fb3426a716af
Gordon Watts <gwatts@uw.edu>
2025-05-26 22:01:12 -0700

    Flesh out table, and sort it (#4)
    * Print min/max per event.
    
    * Sort order the output table.

--- src/atlas_mc_scanner/list_particles.py
+++ src/atlas_mc_scanner/list_particles.py
@@ -83,14 +83,35 @@ def execute_request(ds_name):
     unique, counts = np.unique(r, return_counts=True)
     pdgid_counts = dict(zip(unique, counts))
 
+    # Lets calculate the max and min particle counts for each particle id.
+    count = {pid: ak.count(result.pdgid[result.pdgid == pid], axis=1) for pid in unique}
+    max_count = {pid: ak.max(count[pid]) for pid in unique}
+    min_count = {pid: ak.min(count[pid]) for pid in unique}
+
+    # Build and print final table.
     table = [
-        (pid, get_particle_name(pid), count, count / total_events)
+        (
+            pid,
+            get_particle_name(pid),
+            count,
+            count / total_events,
+            max_count[pid],
+            min_count[pid],
+        )
         for pid, count in pdgid_counts.items()
     ]
+    table.sort(key=lambda x: x[2], reverse=True)
     print(
         tabulate(
             table,
-            headers=["PDG ID", "Name", "Count", "Avg Count/Event"],
+            headers=[
+                "PDG ID",
+                "Name",
+                "Count",
+                "Avg Count/Event",
+                "Max Count/Event",
+                "Min Count/Event",
+            ],
             tablefmt="fancy_grid",
         )
     )
